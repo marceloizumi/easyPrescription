ee8601c78c46cf0a1a1f9cc10372edb984103c98
marceloizumi <marcelo.cicomp@gmail.com>
2013-11-05 23:37:49 -0200

    Create Receita.java

---
+++ Receita.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package easyPrescription;
+
+import java.util.List;
+import javax.swing.*;
+
+/**
+ * @author MarceloGabriel
+ *
+ */
+public class Receita extends JFrame {
+	
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public List<String> listaMedicamentos;
+	public List<String> listaMedicamentosSelecionados;
+	
+	
+
+}
